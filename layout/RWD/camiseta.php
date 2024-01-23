7c3c9e77f0220afabe492df48631da9512cb5e5f
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-01-23 10:51:00 +0100

    organize code

--- layout/RWD/camiseta.php
+++ layout/RWD/camiseta.php
@@ -2,38 +2,27 @@
 <html dir="ltr" lang="es">
 
 <head>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-    <!--Optimización para dispositivos móviles: OBLIGATORIA-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Desarrollador Multiplataforma y web - Gonzalo Margoton </title>
-    <!--Etiquetas META -->
-    <!--PRESENTACION EN RESULTADOS Y NAVEGADOR-->
-    <!--Para que en los resultados de búsqueda aparezca correctamente representada la página
-                                localizada con título y descripción -->
-    <meta name="title" content="Desarrollador Multiplataforma y web - Gonzalo Margoton">
-    <meta name="description" content="estos son los trabajos que he realizado ">
-    <!--POSICIONAMIENTO-->
-    <meta name="keywords" content="tecnico en sistemas, DAM, DAW, pajinas Web">
-    <meta name="author" content="Gonzalo Margotón">
 
-    <link rel="stylesheet" type="text/css" href="../todo0">
+    <meta charset="utf-8">
+    <link rel="icon" type="image/png" href="/favicon.ico" sizes="16x16"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Layout </title>
+    <meta name="title" content="Layout de Alejandro Manzano">
+    <meta name="description" content="Layout principal">
+    <meta name="keywords" content="HTML, HTML5, CSS, CSS3, JAVASCRIPT, PHP, SQL, Bases de datos, Hease, WEB, Accesibilidad">
+    <meta name="author" content="Alejandro Manzano">
     <link rel="stylesheet" type="text/css" href="../mainS.css">
     <link rel="stylesheet" type="text/css" href="cssCamisetas.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
     <style>
     <?php include '../menucss.css';?> 
     </style>
-
-
-
-    <!-- Scripts que realizan operaciones en la estructura de la página
-                                                    cuando se carga-->
-    <!--<script src="js/YY.js"></script>-->
+  
 </head>
 
 <body>
+
     <span id="arriba"></span>
   
     <header>
@@ -42,59 +31,67 @@
 
     </header>
 
-
     <main>
 
         <section>
+
             <article >
-            <h1>Esta es la de las camisetas</h1>
 
+                <h1>Esta es la de las camisetas</h1>
 
-<div id="ejemplo">Lorem ipsum dolor sit amet consectetur adipiscing elit cursus libero placerat eu metus, sagittis volutpat lobortis parturient ante mauris et vestibulum id tellus faucibus ligula bibendum, phasellus sapien leo fusce ullamcorper luctus condimentum semper magnis auctor vehicula. Velit ullamcorper aptent netus commodo suscipit, facilisi felis phasellus nullam, suspendisse fermentum auctor leo. Vel massa vitae fermentum proin tempus gravida himenaeos lacus sed condimentum facilisi aenean velit suscipit magna urna curabitur, facilisis erat eleifend aliquet sodales maecenas cursus arcu porta quisque praesent sociosqu posuere laoreet felis.
+                <div id="ejemplo">
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit cursus libero placerat eu metus, sagittis volutpat
+                    lobortis parturient ante mauris et vestibulum id tellus faucibus ligula bibendum, phasellus sapien leo fusce
+                    ullamcorper luctus condimentum semper magnis auctor vehicula. Velit ullamcorper aptent netus commodo suscipit, 
+                    facilisi felis phasellus nullam, suspendisse fermentum auctor leo. Vel massa vitae fermentum proin tempus gravida 
+                    himenaeos lacus sed condimentum facilisi aenean velit suscipit magna urna curabitur, facilisis erat eleifend aliquet
+                    sodales maecenas cursus arcu porta quisque praesent sociosqu posuere laoreet felis.
+                    Integer volutpat in hac venenatis facilisi habitant dapibus quis ultricies, aliquet lacinia ullamcorper fames sapien
+                    fermentum vulputate pharetra, convallis viverra montes nunc posuere egestas faucibus sociosqu. Ridiculus massa dis vehicula 
+                    consequat imperdiet augue ac blandit, feugiat ornare orci nunc eget erat sed ullamcorper at, morbi sollicitudin aptent dignissim
+                    dui velit curae. Commodo magna posuere auctor etiam vulputate at eu sociis est sodales, suspendisse interdum velit lectus nec condimentum 
+                    elementum aliquet accumsan dictumst, dis porttitor cursus tellus turpis facilisi non malesuada pretium. Penatibus arcu consequat curabitur 
+                    praesent gravida mi ad tortor ullamcorper rutrum platea scelerisque, dignissim parturient vitae risus non accumsan quis feugiat mollis tempus.
+                    Pharetra est laoreet malesuada neque aliquet cursus, nullam himenaeos vitae aliquam sollicitudin, quisque auctor non sodales orci. Fringilla morbi
+                    odio neque mattis massa ligula leo turpis ridiculus, mollis sollicitudin proin bibendum platea feugiat volutpat rutrum cursus lobortis, 
+                    nam habitant pellentesque himenaeos per enim aliquet quam. Congue iaculis leo tempor vitae nisl viverra eleifend habitasse, velit sem purus ac convallis
+                    class commodo quam, auctor laoreet malesuada nascetur egestas dictum turpis.
+                    Fames commodo justo suspendisse tincidunt ridiculus hendrerit dui hac non, rhoncus ultrices ut convallis iaculis cursus tristique elementum nibh nulla, ad 
+                    luctus duis libero donec imperdiet et montes. Vestibulum diam parturient dictumst nec netus habitant viverra taciti himenaeos in posuere, odio nullam placerat 
+                    dui nulla magna lacus habitasse mi ad. Augue lacus risus aptent parturient consequat sagittis taciti congue hendrerit, iaculis orci convallis faucibus tempus etiam nunc himenaeos sapien, ut purus laoreet mauris ridiculus eget eu dictum. 
+                 </div>
 
-Integer volutpat in hac venenatis facilisi habitant dapibus quis ultricies, aliquet lacinia ullamcorper fames sapien fermentum vulputate pharetra, convallis viverra montes nunc posuere egestas faucibus sociosqu. Ridiculus massa dis vehicula consequat imperdiet augue ac blandit, feugiat ornare orci nunc eget erat sed ullamcorper at, morbi sollicitudin aptent dignissim dui velit curae. Commodo magna posuere auctor etiam vulputate at eu sociis est sodales, suspendisse interdum velit lectus nec condimentum elementum aliquet accumsan dictumst, dis porttitor cursus tellus turpis facilisi non malesuada pretium. Penatibus arcu consequat curabitur praesent gravida mi ad tortor ullamcorper rutrum platea scelerisque, dignissim parturient vitae risus non accumsan quis feugiat mollis tempus.
+                <div id="ejemplo2">
 
-Pharetra est laoreet malesuada neque aliquet cursus, nullam himenaeos vitae aliquam sollicitudin, quisque auctor non sodales orci. Fringilla morbi odio neque mattis massa ligula leo turpis ridiculus, mollis sollicitudin proin bibendum platea feugiat volutpat rutrum cursus lobortis, nam habitant pellentesque himenaeos per enim aliquet quam. Congue iaculis leo tempor vitae nisl viverra eleifend habitasse, velit sem purus ac convallis class commodo quam, auctor laoreet malesuada nascetur egestas dictum turpis.
+                    <div class="cajas">
+                        <p>1</p>
+                    </div>
 
-Fames commodo justo suspendisse tincidunt ridiculus hendrerit dui hac non, rhoncus ultrices ut convallis iaculis cursus tristique elementum nibh nulla, ad luctus duis libero donec imperdiet et montes. Vestibulum diam parturient dictumst nec netus habitant viverra taciti himenaeos in posuere, odio nullam placerat dui nulla magna lacus habitasse mi ad. Augue lacus risus aptent parturient consequat sagittis taciti congue hendrerit, iaculis orci convallis faucibus tempus etiam nunc himenaeos sapien, ut purus laoreet mauris ridiculus eget eu dictum.  </div>
+                    <div class="cajas">
+                        <p>2</p>
+                    </div>
 
-<div id="ejemplo2">
+                    <div class="cajas">
+                        <p>3</p>
+                    </div>
 
-<!-- Segunda caja contenedora y dentro otras cuatro -->
+                    <div class="cajas">
+                        <p>4</p>
+                    </div>
 
-<div class="cajas">
+                </div>
 
-    <p>1</p>
-</div>
-
-<div class="cajas">
-
-<p>2</p>
-</div>
-
-<div class="cajas">
-
-<p>3</p>
-</div>
-
-<div class="cajas">
-
-<p>4</p>
-</div>
-
-</div>
             </article>
+
         </section>
 
         <a href="#arriba" title="Pulsa para ir al comienzo de la página"></a>
 
-        <footer style="color: white;">&copy;GM</footer>
+        <footer style="color: white;">GM</footer>
+
     </main>
 
 
 </body>
-<!-- Scripts que no realizan operaciones en la estructura de la página cuando se carga-->
-<!-- <scr ipt src="js/xx. js">
-        </script>-->
 
 </html>
