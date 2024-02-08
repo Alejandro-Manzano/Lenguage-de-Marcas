cf499bb0b08c12a96c2bec04d7c8aaada50c10d7
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-02-08 13:59:17 +0100

    new menu

---
+++ layout/flexbox/Coches.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+    
+        <meta charset="utf-8">
+        <link rel="icon" type="image/png" href="porsche.ico" sizes="16x16"/>
+        <meta name="language" content="es" />
+        <title>Coches</title>
+        <meta name="Title" content= "DAM, JAVASCRIPT, PHP, HTML, CSS, XAMPP" />
+        <meta name="description" content="Coches" />
+        <meta name="keywords" content="HTML, HTML5, CSS, CSS3, JAVASCRIPT, PHP, SQL, Bases de datos, Hease, WEB, Accesibilidad" />
+        <meta name="distribution" content="global" />
+        <meta name="author" content="Alejandro Manznao" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="normalize.css">
+        <link rel="stylesheet" href="Coches.css">
+
+        <style>
+            <?php include '../menucss.css';?> 
+        </style>
+
+    </head>
+
+    <body>
+
+       <!--  <header>
+        <?php include '../../layout/menucss.php';?> 
+        </header> -->
+
+        <main>
+
+            <div class="container-one">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Porsche_hood_emblem.png/457px-Porsche_hood_emblem.png?20170204060946" alt="porsche-logo" class="logo">
+                <h1>Modelos 2023/24</h1>
+            </div>
+
+            <div class="container-two">
+                <div>
+                    <img src="https://configurator.porsche.com/model-start/pictures/911/extcam03.webp" alt="911" class="car">
+                    <p></p>
+                    <a href=""></a>
+                </div>
+                <div>
+                    <img src="https://configurator.porsche.com/model-start/pictures/taycan/extcam03.webp" alt="taycan" class="car">
+                    <p></p>
+                    <a href=""></a>
+                </div>
+                <div>
+                    <img src="https://configurator.porsche.com/model-start/pictures/macan/extcam03.webp" alt="macan" class="car">
+                    <p></p>
+                    <a href=""></a>
+                </div>
+                <div>
+                    <img src="https://configurator.porsche.com/model-start/pictures/panamera/extcam03.webp" alt="panamera" class="car">
+                    <p></p>
+                    <a href=""></a>
+                </div>
+            </div>
+
+        </main>
+
+    </body>
+
+</html>
--- layout/menucss.php
+++ layout/menucss.php
@@ -26,7 +26,7 @@
               <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content">
-              <a href="../../css/flexbox/ACT-1-coches-peugeot.php">Coches</a>
+              <a href="../../layout/flexbox/Coches.php">Coches</a>
               <a href="../../css/flexbox/protonvpn.php">Proton VPN</a>
               <a href="../../css/flexbox/operadora-flex.php">Operadora</a>
               
