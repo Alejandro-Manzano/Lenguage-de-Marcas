7b9786c6521cb1ba9819a616fcd2b0184282924e
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-04-30 10:49:07 +0200

    quijote txt

--- javascript/7-ALAX-LecturaTxT/ajax-txt.php
+++ javascript/7-ALAX-LecturaTxT/ajax-txt.php
@@ -3,20 +3,23 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="language" content="es" >
-    <title>| DAM | - JAVASCRIPT</title>
-    <meta name="Title" content="| DAM | - JAVASCRIPT" >
-    <meta name="description" content="Ejercicicios utilizando tecnologías WEB en el cliente" >
-    <meta name="keywords" content="HTML, HTML5, CSS, CSS3, JAVASCRIPT, PHP, SQL, Bases de datos, parallax, WEB, Accesibilidad" >
-    <meta name="distribution" content="global" >
-    <meta name="author" content="Vuestro nombre" >
+    <meta name="language" content="es">
+    <title>Ajax TXT</title>
+    <link rel="icon" type="image/png" href="../js.webp" sizes="16x16" />
+    <meta name="Title" content="| DAM | - JAVASCRIPT">
+    <meta name="description" content="Ejercicicios utilizando tecnologías WEB en el cliente">
+    <meta name="keywords"
+        content="HTML, HTML5, CSS, CSS3, JAVASCRIPT, PHP, SQL, Bases de datos, Parallax, WEB, Accesibilidad">
+    <meta name="distribution" content="global">
+    <meta name="author" content="Alejandro Manzano">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <!--    Animaciones CSS https://daneden.github.io/animate.css/-->
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
-
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Brygada+1918:ital,wght@0,400..700;1,400..700&display=swap"
+        rel="stylesheet">
 
     <link rel="stylesheet" href="../js.css">
 
@@ -24,46 +27,233 @@
 
     <!--Estilos-->
     <style>
-        
+    body {
+        height: 1200px;
+    }
+
+    #contenido {
+        width: 650px;
+    }
+
+    #contenedor {
+        margin-top: 0px;
+        margin-right: auto;
+        margin-bottom: 10px;
+        margin-left: auto;
+    }
+
+    #contenedor #contenido {
+        width: 580px;
+        border: none;
+        position: relative;
+        text-align: center;
+        margin: auto;
+    }
+
+    #resultado2 {
+        font-family: "Brygada 1918", serif;
+        font-optical-sizing: auto;
+        font-weight: <weight>;
+        font-style: normal;
+        position: absolute;
+        left: 40px;
+        top: 450px;
+        width: 513px;
+        height: 323px;
+        z-index: 1;
+        border: 4px groove #666;
+        padding: 10px;
+        overflow: auto;
+        text-align: left;
+        font-size: 1.3em;
+    }
+
+    #resultado2 p {
+        text-indent: 50px;
+    }
+
+
+    .letra:first-letter {
+        color: #666666;
+        display: block;
+        /* Lo convertimos en bloque */
+        float: left;
+        /*Lo mandamos a la izquierda */
+        font-size: 300%;
+        /* Hacemos crecer la primera letra */
+        margin: 10px;
+        /* Y le ponemos un margen cuadrado de 5 píxeles */
+    }
     </style>
+    <script type="text/javascript">
+    function iniciar() {
+
+        var conexion;
+
+        conexion = new XMLHttpRequest();
+
+        //Creamos el Objeto y comprobamos que es soportado por el navegador y en caso contrario estamos ante una versión antigua de IExplorer y procedemos a cargarlo como control Active X
+
+        if (window.XMLHttpRequest) {
+
+            conexion = new XMLHttpRequest();
+
+        } else {
+            //para versiones obsoletas de IExplorer
+
+            conexion = new ActiveXObject("Microsoft.XMLHTTP");
+
+        }
+
+        conexion.onreadystatechange = function() {
+
+            if (conexion.readyState == 4 && conexion.status == 200) {
+
+                //Contendor de respuesta
+
+                document.getElementById("resultado2").innerHTML = conexion.responseText;
+            }
+
+        }
+
+
+        //Una vez que ya tenemos el objeto creado procedemos a la apertura y envío de la peticion
+
+        conexion.open("GET", "quijote.txt", true);
+        conexion.send();
+    }
+    </script>
+
 </head>
 
 <body>
     <main class="bgimg-1 animated fadeIn">
         <nav class="topnav" id="myTopnav">
 
-        <?php include '../menu.php'; ?>
-      
-    </nav>
+
+            <!-- <i class="fa fa-home x2" aria-hidden="true"></i> -->
+
+            <b><a href="../index.html">GLOBAL</a></b>
+
+            <a href="../index.html">Inicio</a>
+
+            <div class="dropdown">
+
+                <button class="dropbtn">Técnicos
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="../0-Abono/abono.php">Abono Transporte</a>
+                    <a href="../1-Tombola/tombola.php">¡Tombola!</a>
+                    <a href="../2-Exploradores/exploradores.php">Exploradores</a>
+                    <a href="../3-Mensajeria/mensajeria.php">Mensajería</a>
+                    <a href="../4-Texto/texto.php">Mayus/Minus</a>
+                    <a href="../5-Chocolate/chocolate.php">Chocolate</a>
+                    <a href="../6-Procesador-de-Textos/procesador.php">Procesador de Textos</a>
+                    <a href="../7-ALAX-LecturaTxT/ajax-txt.php">AJAX-Txt</a>
+                    <a href="../8-LecturaXML/ajax-xml.php">Ajax & Xml</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Box
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="../LightBox/lightbox.php">LightBox</a>
+                    <a href="../ShadowBox/shadowbox.php">ShadowBox</a>
+
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <button class="dropbtn">Sliders
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="../Slider-1/slider-1.php">Modelo 1</a>
+                    <a href="../Slider-2/slider-2.php">Modelo 2</a>
+
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <button class="dropbtn">Galerías
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="../Galeria-1/Galeria-1.php">Modelo 1</a>
+                    <a href="../Galeria-2/Galeria-2.php">Modelo 2</a>
+
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Tiempo
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="../Reloj/reloj.php">Reloj</a>
+                    <a href="../Datepicker/datepicker.php">DatePicker</a>
+                    <a href="../CountDown/countdown.php">CountDown</a>
+                </div>
+            </div>
+
+            <a href="../parallax/parallax.php">Parallax</a>
+            <a href="../jquery/jquery.php">JQuery</a>
+            <!--
+<div class="dropdown">
+                <button class="dropbtn">Juegos
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="game-1.html">Game - I</a>
+                    <a href="game-2.html">Game - II</a>
+                    
+                </div>
+            </div>
+-->
+
+            <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+
+        </nav>
 
         <!--A partir de aqui el contenido-->
 
         <div>
             <div class="caption animated zoomInDown">
                 <span class="border">Lectura de TXT con ajax</span><br>
-<!--                <span class="border">-->
-                    
-                    <div id="contenido">
-                    
-                    <h1>Contenido de cada script aqui</h1>
-                    
+
+                <div id="contenido">
+                    <div id="contenedor">
+                        <div id="contenido">
+                            <div class="letra" id="resultado2"></div>
+                            <p><img src="fondo-quijote.png" width="580" height="775"></p>
+                            <button type="button" onclick="iniciar()">Cargar Texto (AJAX) >></button>
+                            <p>*Texto obtenido de <a href="http://www.quijotipsum.com/"
+                                    target="_blank">http://www.quijotipsum.co m/</a></p>
+                        </div>
+
+                        <div id="resultado"></div>
                     </div>
-                
-<!--                </span>-->
+                </div>
+
+
             </div>
 
         </div>
-        <footer>Fuente: <a href="#" class="enlacepie">Dirección URL de la web donde esté el Script</a></footer>
+        <footer>Fuente: <a href="https://javascript.solired.es/051-AJAX/2-quijote.html" class="enlacepie">Dirección URL
+                de la web donde esté el Script</a></footer>
     </main>
     <script>
-        function myFunction() {
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
-                x.className += " responsive";
-            } else {
-                x.className = "topnav";
-            }
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
         }
+    }
     </script>
 
-</body></html>
+</body>
+
+</html>
